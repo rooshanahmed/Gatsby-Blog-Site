6c8917ae3e20fa80473700f7193a565129a71df8
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-12-07 00:56:08 +0500

    Create blog.tsx

---
+++ src/pages/blog.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../layout/Layout"
+
+export default function Blogs() {
+  return (
+    <div>
+      <Layout title="Blogs Hub">
+          
+      </Layout>
+    </div>
+  )
+}
