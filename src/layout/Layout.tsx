22c4dd975f4e8fa440a400072191726ac383f519
Rooshan <rooshan.ahmed@hotmail.com>
2020-12-07 02:02:32 +0500

    Update Layout.tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,5 @@
+import React, { ReactNode } from "react"
 import { CssBaseline } from "@material-ui/core"
-import React from "react"
-import { ReactNode } from "react"
 import Header from "../components/Header"
 
 export interface layoutProps {
