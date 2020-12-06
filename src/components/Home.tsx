38dd90611c3efc5bbc579b7073a64ed6d80ecc46
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-12-06 23:29:30 +0500

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,2 +1,19 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core'
+import { createStyles, makeStyles, Theme, Typography, Button } from '@material-ui/core'
+
+const useStyles = makeStyles((theme: Theme) => createStyles({}));
+
+const Home = () => {
+    const classes = useStyles();
+
+    return(
+        <div>
+            <img src='../../static/landing.png' alt='gatsby blog' />
+            <div>
+                <Typography>Welcome To The Blogs Hub</Typography>
+                <Button>Go To Blogs</Button>
+            </div>
+        </div>
+    )
+}
+export default Home;
