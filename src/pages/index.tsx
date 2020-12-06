1bb79421baed25af8beefb5c561a2690886423f6
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-12-07 00:21:00 +0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
 import React from "react"
+import Home from "../components/Home"
+import Layout from "../layout/Layout"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function HomePage() {
+  return(
+    <div>
+      <Layout title="Blogs Hub">
+        <Home />
+      </Layout>
+    </div>
+  )
 }
