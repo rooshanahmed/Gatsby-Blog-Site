84e5b8b80caaa906e15d189594068deedc1d00da
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-12-07 00:20:38 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,9 +17,4 @@ module.exports = {
       },
     },
   ],
-  siteMetadata: {
-    title: `Gatsby`,
-    siteUrl: `https://www.gatsbyjs.com`,
-    description: `Blazing fast modern site generator for React`,
-  },
 }
