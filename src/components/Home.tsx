59b3e722c89aae7b8e70ad2e96751bf90a37f0d4
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-12-07 00:56:00 +0500

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,49 +1,60 @@
-import React from 'react'
-import { createStyles, makeStyles, Theme, Typography, Button } from '@material-ui/core'
-import bg1 from '../../static/bg1.svg'
+import React from "react"
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography,
+  Button,
+} from "@material-ui/core"
+import bg1 from "../../static/bg1.svg"
+import { navigate } from "gatsby"
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
     root: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#280B0B',
-        minHeight: '100vh'
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#280B0B",
+      minHeight: "100vh",
     },
     img: {
-        width: '450px',
-        marginRight: '150px',
+      width: "450px",
+      marginRight: "150px",
     },
     text: {
-        color: '#fff',
-        fontSize: '44px',
-        fontFamily: 'Comfortaa',
+      color: "#fff",
+      fontSize: "44px",
+      fontFamily: "Comfortaa",
     },
     btn: {
-        color: '#fff',
-        marginTop: '20px',
-        width: '180px',
-        border: '2px solid black',
-        marginLeft: '155px',
-        fontFamily: 'Comfortaa',
-        textTransform: 'capitalize',
-        '&:hover': {
-            backgroundColor: 'black',
-        }
-    }
-}));
+      color: "#fff",
+      marginTop: "20px",
+      width: "180px",
+      border: "2px solid black",
+      marginLeft: "155px",
+      fontFamily: "Comfortaa",
+      textTransform: "capitalize",
+      "&:hover": {
+        backgroundColor: "black",
+      },
+    },
+  })
+)
 
 const Home = () => {
-    const classes = useStyles();
+  const classes = useStyles()
 
-    return(
-        <div className={classes.root}>
-            <img src={bg1} alt='gatsby blog' className={classes.img} />
-            <div>
-                <Typography className={classes.text}>Welcome To Blogs Hub</Typography>
-                <Button className={classes.btn} disableTouchRipple>Go To Blogs</Button>
-            </div>
-        </div>
-    )
+  return (
+    <div className={classes.root}>
+      <img src={bg1} alt="gatsby blog" className={classes.img} />
+      <div>
+        <Typography className={classes.text}>Welcome To Blogs Hub</Typography>
+        <Button className={classes.btn} disableTouchRipple onClick={() => {navigate('/blog')}}>
+          Go To Blogs
+        </Button>
+      </div>
+    </div>
+  )
 }
-export default Home;
+export default Home
