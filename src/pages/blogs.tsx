b90d50da8c714c42bcaba2fec2f36597955533f1
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-12-07 12:28:00 +0500

    Update blogs.tsx

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -1,26 +1,83 @@
-import React from "react";
-import { graphql } from "gatsby";
-import Layout from "../layout/Layout";
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../layout/Layout"
+import {
+  Theme,
+  makeStyles,
+  createStyles,
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+} from "@material-ui/core"
 
-export default function Blogs() {
+export interface BlogListProps {
+  data: any
+}
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  root: {
+    maxWidth: 1200,
+  },
+}))
+
+export const Blogs = ( data: any ) => {
+  const classes = useStyles()
+  const blog = data.node;
+
   return (
     <div>
-      <Layout title="Blogs Hub">
-        <div></div>
-      </Layout>
+      <Grid container>
+        <Grid container xs={12}>
+          <Grid item xs={12}>
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Blog Post"
+                  height="400"
+                  image={blog.mediaImages.file.url}
+                />
+                <CardContent>
+                  <Typography>{blog.title}</Typography>
+                  <Typography>{blog.miniTitle}</Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
-  );
+  )
 }
+
+const BlogList: React.SFC<BlogListProps> = ({ data }) => {
+  const blogs = data.allContentfulBlogPost.edges
+
+  return (
+    <Layout title="Blogs Hub">
+      <h1>Blogs</h1>
+      {blogs.map((blog: any) => {
+        return( Blogs(blog) )
+      })}
+    </Layout>
+  )
+}
+export default BlogList;
 
 export const query = graphql`
   query {
     allContentfulBlogPost {
-      nodes {
-        title
-        miniTitle
-        mediaImages {
-          file {
-            url
+      edges {
+        node {
+          title
+          miniTitle
+          mediaImages {
+            file {
+              url
+            }
           }
         }
       }
