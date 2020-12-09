a050c62e2071ce22fcfedfd9ac2f4fec989f6be0
Rooshan <rooshan.ahmed@hotmail.com>
2020-12-09 15:25:50 +0500

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -24,7 +24,7 @@ const useStyles = makeStyles((theme: Theme) =>
       width: "450px",
       marginRight: "290px",
       [theme.breakpoints.down("md")]: {
-        marginTop: "70px",
+        marginTop: "30px",
         marginRight: "10px",
         width: "250px",
       },
