47b6a3c63dc0073a69505f885912bfe95b605b09
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-12-11 00:23:11 +0500

    Update blog.tsx

--- src/templates/blog.tsx
+++ src/templates/blog.tsx
@@ -1,55 +1,61 @@
-import React from 'react'
-import { createStyles, makeStyles, Theme, Typography } from '@material-ui/core'
-import Layout from '../layout/Layout'
-import moment from 'moment';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import React from "react"
+import { createStyles, makeStyles, Theme, Typography } from "@material-ui/core"
+import Layout from "../layout/Layout"
+import moment from "moment"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 export interface BlogProps {
-    pageContext: any
+  pageContext: any
 }
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
     root: {
-        marginTop: '120px',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
+      marginTop: "120px",
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center",
     },
     title: {
-        fontFamily: 'Comfortaa',
-        fontSize: '54px',
-        fontWeight: 'bold'
+      fontFamily: "Comfortaa",
+      fontSize: "54px",
+      fontWeight: "bold",
     },
     subText: {
-        fontWeight: 'bold',
-        fontSize: '20px',
-        marginTop: '20px',
-        marginBottom: '20px',
+      fontWeight: "bold",
+      fontSize: "20px",
+      marginTop: "20px",
+      marginBottom: "20px",
     },
     image: {
-        [theme.breakpoints.down("md")]:{
-            width: "240px",
-            height: "150px",
-        }
-    }
-}));
+      [theme.breakpoints.down("md")]: {
+        width: "240px",
+        height: "150px",
+      },
+    },
+  })
+)
 
 const BlogPage: React.SFC<BlogProps> = ({ pageContext }) => {
-    const classes = useStyles();
-    const blog = pageContext;
-    
-    return(
-        <div>
-            <Layout title="Blogs Hub">
-                <div className={classes.root}>
-                    <Typography className={classes.title}>{blog.title}</Typography>
-                    <p>{moment(blog.publishedDate, 'YYYY MM DD hh:mm').fromNow()}</p>
-                    <Typography>{documentToReactComponents(JSON.parse(blog.body))}</Typography>
-                    <img src={blog.imageUrl} alt={blog.title} className={classes.image} />
-                    <Typography className={classes.subText}>Author: {blog.author}</Typography>
-                </div>
-            </Layout>
+  const classes = useStyles()
+  const blog = pageContext
+
+  return (
+    <div>
+      <Layout title="Blogs Hub">
+        <div className={classes.root}>
+          <Typography className={classes.title}>{blog.title}</Typography>
+          <p>{moment(blog.publishedDate, "YYYY MM DD hh:mm").fromNow()}</p>
+          <Typography>
+            {documentToReactComponents(JSON.parse(blog.body))}
+          </Typography>
+          <img src={blog.imageUrl} alt={blog.title} className={classes.image} />
+          <Typography className={classes.subText}>
+            Author: {blog.author}
+          </Typography>
         </div>
-    )
+      </Layout>
+    </div>
+  )
 }
-export default BlogPage;
+export default BlogPage
